46e841ede1c96e1a20ff5799df73fa0f35b0c7d4
Arav Jain <aravhawk@gmail.com>
2024-06-09 18:28:37 +0530

    Fix home.py URL forwarding

--- home.py
+++ home.py
@@ -4,4 +4,4 @@ st.title("NeuralBytes Search")
 st.write('The open-source AI "search engine". Controlled by you. Owned by you. No big-tech spying.')
 
 query = st.text_input("What would you like to know?")
-st.page_link(f"http://localhost:8501/search?q={query}", label="Search", icon="🔎", disabled=False)
+st.page_link(f"https://neuralbytes-search.streamlit.app/search?q={query}", label="Search", icon="🔎", disabled=False)
