59c33e1723e2030f4608df9b8915ace50eb067d9
Arav Jain <aravhawk@gmail.com>
2024-07-05 03:12:58 +0530

    NeuralBytes Search v1.0.3

--- home.py
+++ home.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title=f"NeuralBytes Search",
+    page_icon=":mag:",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get help': 'mailto:support@neuralbytes.net?subject=Need%20Help%20with%20NeuralBytes%20Search',
+        'Report a bug': 'mailto:bugs@neuralbytes.net?subject=NeuralBytes%20Search%20Bug%20Report',
+        'About': '''### NeuralBytes Search: The open-source AI "search engine". Controlled by you. Owned by you. No big-tech spying. \n
+        https://neuralbytes-search.streamlit.app'''
+    }
+)
+
 st.title("NeuralBytes Search")
 st.write('The open-source AI "search engine". Controlled by you. Owned by you. No big-tech spying.')
+st.write("[Help improve NeuralBytes Search](mailto:feedback@neuralbytes.net?subject=AlphaPredict%20Feedback)")
 
 query = st.text_input("What would you like to know?")
-st.page_link(f"https://neuralbytes-search.streamlit.app/search?q={query}", label="Search", icon="🔎", disabled=False)
+st.page_link(f"https://neuralbytes-search.streamlit.app/search?q={query}", label="Search", icon="🔍", disabled=False)
--- pages/search.py
+++ pages/search.py
@@ -1,22 +1,37 @@
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-sys_prompt = """You are the NeuralBytes Search Bot, a ChatGPT-3.5-based AI assistant, mainly functioning as a search
-engine. IMPORTANT: DO NOT TRY BOLDING, ITALICIZING, APPLYING SPECIAL FONTS, EFFECTS, ETC. TO TEXT. DON'T PUT _<TEXT>_
-*<TEXT>* `<TEXT>` OR ANYTHING SIMILAR, AS THE STREAMLIT WEB INTERFACE HATES (SUCKS AT) WORKING WITH TEXT FORMATTING AND 
-MARKDOWN."""
-
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
 if st.query_params['q'].strip(" ") is not None:
     query = st.query_params['q']
+
+    st.set_page_config(
+        page_title=f"{query} - NeuralBytes Search",
+        page_icon=":mag:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+        menu_items={
+            'Get help': 'mailto:support@neuralbytes.net?subject=Need%20Help%20with%20NeuralBytes%20Search',
+            'Report a bug': 'mailto:bugs@neuralbytes.net?subject=NeuralBytes%20Search%20Bug%20Report',
+            'About': '''### NeuralBytes Search: The open-source AI "search engine". Controlled by you. Owned by you. No big-tech spying. \n
+            https://neuralbytes-search.streamlit.app'''
+        }
+    )
+
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+    tokens_limit = 500
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+
+    sys_prompt = f"""You are the NeuralBytes Search Bot, a ChatGPT-3.5-based AI assistant, mainly functioning as a search
+    engine. IMPORTANT: DO NOT TRY BOLDING, ITALICIZING, APPLYING SPECIAL FONTS, EFFECTS, ETC. TO TEXT. DON'T PUT _<TEXT>_
+    *<TEXT>* `<TEXT>` OR ANYTHING SIMILAR, AS THE STREAMLIT WEB INTERFACE HATES (SUCKS AT) WORKING WITH TEXT FORMATTING AND 
+    MARKDOWN. ALSO, MAX TOKENS FOR RESPONSE: {tokens_limit}"""
+
+    for message in st.session_state["messages"]:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
     st.session_state["messages"].append({"role": "user", "content": query})
     with st.chat_message("user"):
         st.markdown(query)
@@ -33,7 +48,7 @@ if st.query_params['q'].strip(" ") is not None:
                              {"role": m["role"], "content": m["content"]} for m in st.session_state["messages"]
                          ],
                 stream=True,
-                max_tokens=300
+                max_tokens=tokens_limit
         ):
             incremental_content = response.choices[0].delta.content or ""
             full_response += incremental_content
@@ -42,3 +57,5 @@ if st.query_params['q'].strip(" ") is not None:
         message_placeholder.markdown(full_response)
 
     st.session_state["messages"].append({"role": "assistant", "content": full_response})
+else:
+    st.stop()
